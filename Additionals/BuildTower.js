d0d3ee0cf855689824360fdc477d31f3ca6bc634
feden2906 <feden2906@gmail.com>
2022-01-06 09:41:13 +0200

    add new additional task

---
+++ Additionals/BuildTower.js
@@ -0,0 +1,19 @@
+// Build Tower
+//
+// Build Tower by the following given argument:number of floors (integer and always greater than 0).
+// for example, a tower of 3 floors looks like below
+//     [
+//     '  *  ',
+//         ' *** ',
+//         '*****'
+//     ]
+//
+// and a tower of 6 floors looks like below
+//     [
+//         '     *     ',
+//         '    ***    ',
+//         '   *****   ',
+//         '  *******  ',
+//         ' ********* ',
+//         '***********'
+//     ]
