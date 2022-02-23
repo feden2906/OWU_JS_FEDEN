7b98b7f9f5e45822057b8360a641b350c603a396
feden2906 <feden2906@gmail.com>
2022-02-23 16:40:50 +0200

    add stopwatch

--- Additionals/stopwatch.js
+++ Additionals/stopwatch.js
@@ -5,3 +5,19 @@
 // 00 : 00 : 02
 // 00 : 00 : 03
 // ...
+
+const helper = (num) => num.toString().length < 2 ? '0' + num : num;
+
+const stopwatch = (hourOut, minOut, secOut) => {
+  for (let hour = 0; hour <= hourOut; hour++) {
+    for (let min = 0; min < 60; min++) {
+      for (let sec = 0; sec < 60; sec++) {
+        console.log(`${helper(hour)} : ${helper(min)} : ${helper(sec)}`);
+        if (hour === hourOut && min === minOut && sec === secOut) return;
+      }
+    }
+  }
+};
+
+stopwatch(0, 0, 7);
+
