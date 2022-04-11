753d5b24e57211c17af50fc0a44c80c690422a36
feden2906 <feden2906@gmail.com>
2022-04-12 00:46:08 +0300

    recursive

--- Additionals/recursive.js
+++ Additionals/recursive.js
@@ -28,3 +28,24 @@
 //   }
 // }
 
+const inputDataArray = [
+  { parentName: null, itemName: 'programming' }, { parentName: 'mobile', itemName: 'ios' },
+  { parentName: 'mobile', itemName: 'android' }, { parentName: 'backend', itemName: 'nodejs' },
+  { parentName: 'frontend', itemName: 'angular' }, { parentName: 'frontend', itemName: 'react' },
+  { parentName: 'programming', itemName: 'web' }, { parentName: 'frontend', itemName: 'vue' },
+  { parentName: 'backend', itemName: 'nodejs' }, { parentName: 'web', itemName: 'backend' },
+  { parentName: 'programming', itemName: 'mobile' }, { parentName: 'web', itemName: 'frontend' }
+];
+
+const getResult = (inputDataArray, parentName) => {
+  let resultObj = {};
+  for (let i = 0; i < inputDataArray.length; i++) {
+    if (inputDataArray[i].parentName === parentName) {
+      resultObj[inputDataArray[i].itemName] = getResult(inputDataArray, inputDataArray[i].itemName);
+    }
+  }
+  return resultObj;
+}
+
+const result = getResult(inputDataArray, null);
+console.log(result);
