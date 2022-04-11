9d270daa19d7fa635a6ab9d218966621d7a49186
feden2906 <feden2906@gmail.com>
2022-04-12 00:55:56 +0300

    reduce

---
+++ Additionals/reduce.js
@@ -0,0 +1,32 @@
+// Дано масив користувачів, потрібно погрупувати їх за технологіями якими вони володіють
+// const list = [
+//   { name: 'Vika', course: 'REACT' },
+//   { name: 'Viktoria', course: 'JS' },
+//   { name: 'Maks', course: 'REACT' },
+//   { name: 'Tamara', course: 'JAVA' },
+//   { name: 'Volodymyr', course: 'JS' },
+//   { name: 'Maks', course: 'JS' },
+//   { name: 'Alina', course: 'REACT' },
+//   { name: 'Olena', course: 'JS' },
+//   { name: 'Dima', course: 'JAVA' }
+// ];
+//
+// Вихідний обєкт має виглядати так :
+// const result = {
+//   REACT: [
+//     { name: 'Vika', course: 'REACT' },
+//     { name: 'Maks', course: 'REACT' },
+//     { name: 'Alina', course: 'REACT' }
+//   ],
+//   JS: [
+//     { name: 'Viktoria', course: 'JS' },
+//     { name: 'Volodymyr', course: 'JS' },
+//     { name: 'Maks', course: 'JS' },
+//     { name: 'Olena', course: 'JS' }
+//   ],
+//   JAVA: [
+//     { name: 'Tamara', course: 'JAVA' },
+//     { name: 'Dima', course: 'JAVA' }
+//   ]
+// }
+
